c685bebedcdbb6b50d4af5366980b757f9f0bbe9
Sam Brown <jumpbro@hotmail.com>
2023-01-24 17:38:37 +1000

    Setting up an app

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1) Import the React & ReactDOM Libraries
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2) Get a reference to the div with ID root
+// 3) Tell React to take control of that element
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// 4) Create a component
+function App(){
+    const person = { name: "sam"}
+    return (
+        <h1>
+            {new Date().toLocaleTimeString()}
+            
+        </h1>
+    ) 
+
+}
+
+// 5) Show the component on the screen
+root.render(<App />)
