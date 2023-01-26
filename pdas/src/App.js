6fb1fef8f1b1a7b7545329efc82443e03c1cd68d
Sam Brown <jumpbro@hotmail.com>
2023-01-27 08:57:08 +1000

    PDA Project Completed

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css'
 import React from 'react'
 import ProfileCard from './ProfileCard'
 import AlexaImage from '../src/images/alexa.png'
@@ -8,19 +9,46 @@ import CortanaImage from '../src/images/cortana.png'
 export default function App(){
     return(
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard 
-                title = "Alexa" 
-                handle = "@alexa99" 
-                image = {AlexaImage} />
-            <ProfileCard 
-                title = "Cortana" 
-                handle = "cortana32"  
-                image = {CortanaImage} />
-            <ProfileCard 
-                title = "Siri" 
-                handle = "siri01"  
-                image = {SiriImage} />
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">
+                        Personal Digital Assistants
+                    </p>
+                </div>
+            </section>
+            <div className="container">
+                <div className="section">
+                    <div className="columns">
+                        <div className="column is-3">
+                        <ProfileCard 
+                            title = "Alexa" 
+                            handle = "@alexa99" 
+                            image = {AlexaImage} 
+                            description = "Alexa is a personal digital assistant developed by Amazon"
+                        />
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard 
+                                title = "Cortana" 
+                                handle = "cortana32"  
+                                image = {CortanaImage} 
+                                description = "Cortana is a personal digital assistant developed by Microsoft"
+                            />
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard 
+                                title = "Siri" 
+                                handle = "siri01"  
+                                image = {SiriImage} 
+                                description = "Siri is a personal digital assistant developed by Apple"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
