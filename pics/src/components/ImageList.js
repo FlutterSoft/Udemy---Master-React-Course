6b78d379eeed0b0266f9dbb407e4c5a9bf88af1c
Sam Brown <jumpbro@hotmail.com>
2023-01-28 08:12:05 +1000

    Pics project API init

---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,7 @@
+export default function ImageList(){
+    return (
+        <div>
+            ImageList
+        </div>
+    )
+}
---
+++ pics/src/components/ImageShow.js
@@ -0,0 +1,7 @@
+export default function ImageShow(){
+    return (
+        <div>
+            ImageShow
+        </div>
+    )
+}
