8565954c810950533838ac1920fd2cedb3fe4d3c
Sam Brown <jumpbro@hotmail.com>
2023-02-03 08:32:05 +1000

    Created button component and tailwind installed

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,7 +1,22 @@
+import Button from './Button'
 export default function App(){
     return (
-        <div>
-            App
+        <div className="bg-gray-100 h-screen">
+            <div>
+                <Button success>Click</Button>
+            </div>
+            <div>
+                <Button primary>Click Me</Button>
+            </div>
+            <div>
+                <Button warning outline>Click Me Pleaseee</Button>
+            </div>
+            <div>
+                <Button secondary>Please Just Click Meeeeee</Button>
+            </div>
         </div>
+
+            
+
     )
 }
