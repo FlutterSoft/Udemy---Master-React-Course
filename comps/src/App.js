a26e1b74cb0a3a13412da5fb39d56d3c879fbf09
Sam Brown <jumpbro@hotmail.com>
2023-02-08 07:03:18 +1000

    Refactoring with panel component

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,12 +1,31 @@
 import Dropdown from './components/Dropdown'
+import { useState } from 'react'
+
 
 export default function App(){
+    const [selection, setSelection] = useState(null)
+
+    const handleSelection = (option) => {
+        setSelection(option)
+    }
    const options = [
     { label: 'Red', value: 'red' },
     { label: 'Green', value: 'green' },
     { label: 'Blue', value: 'blue' },
    ]
     return (
-        <Dropdown options={options}/>
+        <div className="flex">
+            <Dropdown 
+                options={options}
+                value={selection}
+                onChange={handleSelection}    
+            />
+            <Dropdown 
+                options={options}
+                value={selection}
+                onChange={handleSelection}    
+            />
+        </div>
+
     )
 }
--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,35 +1,39 @@
 import { useState } from 'react'
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+import Panel from './Panel'
 
-export default function Dropdown({ options }){
+
+export default function Dropdown({ options, value, onChange }){
     const [isOpen, setIsOpen] = useState(false)
-    const [selected, setSelected] = useState('')
 
     const handleOptionClick = (option) => {
         setIsOpen(false)
-        setSelected(option)
+        onChange(option)
     }
 
     const handleClick = () => {
         setIsOpen( prev => !prev)
     }
 
+
     const renderedOptions = options.map( (option) => {
         return (
-            <div className="p-5 bg-gray-100 border border-gray-300 cursor-pointer" onClick={() => handleOptionClick(option)} key={option.value}>
+            <div className="p-1 cursor-pointer hover:bg-sky-100" onClick={() => handleOptionClick(option)} key={option.value}>
                 {option.label}
             </div>
         )
-
     })
 
-
     return(
-        <div>
-            <div className="p-5 bg-gray-200 border border-gray-400 cursor-pointer" onClick={handleClick}>{selected.label || "Select"}</div>
+        <div className="w-48 relative">
+            <Panel className="flex justify-between items-center" onClick={handleClick}>
+                {value?.label || 'Select...'}
+                {isOpen ? <GoChevronDown /> :<GoChevronLeft />}
+            </Panel>
             {isOpen &&             
-                <div>
+                <Panel className="absolute top-full divide-y divide-grey-100">
                     {renderedOptions}
-                </div>
+                </Panel>
             }
 
         </div>
