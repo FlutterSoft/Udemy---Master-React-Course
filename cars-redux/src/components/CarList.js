7be5dba0f9b807ebb43cf9bb22c8dc961ffc9791
Sam Brown <jumpbro@hotmail.com>
2023-03-16 08:14:45 +1000

    Added bulma styling, reset inputs on submission, delete buttons, search filtering

--- cars-redux/src/components/CarList.js
+++ cars-redux/src/components/CarList.js
@@ -1,19 +1,31 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeCar } from '../store'
+
 
 // Component to provide a list of cards
 export default function CarList(){
-    const cars = useSelector((state) => {
-        return state.cars.data
+    const dispatch = useDispatch()
+    const cars = useSelector(({ cars: { data, searchTerm }}) => {
+        return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()))
     })
+
+    const handleCarDelete = (car) => {
+        dispatch(removeCar(car.id))
+    }
 
     // Creates a list of car items to render
     const renderedCars = cars.map((car) => {
         return (
-            <div key={car.id} className="panel">
-                <p>
+            <div key={car.id} className="panel is-flex is-justify-content-space-between is-align-items-center p-4">
+                <p className="">
                     {car.name} - ${car.cost}
                 </p>
 
+            <button 
+                className="button is-danger" onClick={() => handleCarDelete(car)}
+            >
+                Delete 
+            </button>
             </div>
         )
     })
