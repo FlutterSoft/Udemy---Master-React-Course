db6513831c5210e494f1b0b6cfa6997b8ad49a30
Sam Brown <jumpbro@hotmail.com>
2023-03-30 08:48:15 +1000

    extraReducers added to watch for fetchUsers and user fetch function

--- media/src/store/slices/usersSlice.js
+++ media/src/store/slices/usersSlice.js
@@ -1,11 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit'
-
+import { fetchUsers } from '../thunks/fetchUsers'
 const usersSlice = createSlice({
     name: 'users',
     initialState: {
-        data: []
+        data: [],
+        isLoading: false,
+        error: null,
     },
-    reducers: {}
+    extraReducers(builder){
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            // Update state object to show user we are loading data
+            state.isLoading = true
+        })
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            // Update state object to show user that we have finished request successfully
+            state.isLoading = false
+            state.data = action.payload
+        })
+        builder.addCase(fetchUsers.error, (state, action) => {
+            // Update state object to show user loading had an error
+            state.isLoading = false
+            state.error = action.error
+
+        })
+    }
 })
 
 export const usersReducer =  usersSlice.reducer
